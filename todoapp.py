23c8706cf39e07e258529a887b0c763aace6a48d
KevinSt <kevinstopper1@gmail.com>
2022-05-25 23:47:01 -0500

    Avance Parte 1

--- todoapp.py
+++ todoapp.py
@@ -1,29 +1,22 @@
 #import libreria Flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-#ruta principal
+headings = ("Tarea","Correo Electronico", "Dificultad", "Descripcion")
+data=(
+    ("Investigar","rahep95022@sceath.com","Media","Investigar Flask")
+    ("Mantenimiento","prollokuwewi-4597@yopmail.com","Alta","Realizar mantenimiento al equipo")
+    ("Encender","zeknudakni@vusra.com","Baja","Encender el ordenador")
+)
+
 @app.route('/')
+def helloworld():
+    return render_template("index.html")
 
-def principal():
-    return render_template('index.html')
+@app.route('/enviar')
+def enviar():
+    return render_template('enviar.html', headings=headings, data=data)
 
-#ruta contacto
-@app.route('/contacto')
-
-#Mensaje ruta de contacto
-def contacto():
-    return render_template('contacto.html')
-
-#ruta de Lenuajes
-@app.route('/lenguajes')
-
-#Mensaje ruta de lenguajes
-def lenguajes():
-    return render_template('lenguajes.html')
-
-#Main del programa
-if __name__ == '__main__':
-    #debug = True, para reiniciar automaticamente el servidor
+if __name__ == "__main__":
     app.run(debug=True)
