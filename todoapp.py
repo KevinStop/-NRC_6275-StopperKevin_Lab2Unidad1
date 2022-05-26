d8e3a3321038beaf35c31d5e8662bec36c670bb3
KevinSt <kevinstopper1@gmail.com>
2022-05-26 00:53:17 -0500

    Modificacion Avance 1

--- todoapp.py
+++ todoapp.py
@@ -1,22 +1,20 @@
 #import libreria Flask
+import email
+from turtle import title
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
-
-headings = ("Tarea","Correo Electronico", "Dificultad", "Descripcion")
-data=(
-    ("Investigar","rahep95022@sceath.com","Media","Investigar Flask")
-    ("Mantenimiento","prollokuwewi-4597@yopmail.com","Alta","Realizar mantenimiento al equipo")
-    ("Encender","zeknudakni@vusra.com","Baja","Encender el ordenador")
-)
 
 @app.route('/')
 def helloworld():
     return render_template("index.html")
 
-@app.route('/enviar')
+@app.route('/enviar', methods=['POST'])
 def enviar():
-    return render_template('enviar.html', headings=headings, data=data)
+    title = request.form['title']
+    return "<td>"+ title +"</td>"
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
