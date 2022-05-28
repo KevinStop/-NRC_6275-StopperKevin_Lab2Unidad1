74a4438d859b6a85d8425836ae0a17301b1f38b3
KevinSt <kevinstopper1@gmail.com>
2022-05-28 18:27:37 -0500

    JavaScript Instanciamiento de constantes

---
+++ templates/main.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', () => {
+	const form = document.querySelector("#new-task-form");
+	const input = document.querySelector("#tarea");
+	const list_el = document.querySelector("#tareas");
+
+	
+});
